6a6209881072a04797ea8efa4526aecd4ac8916d
rom4es <rom4es_1993@mail.ru>
2017-12-24 20:07:46 +0700

    12345

--- js/main.js
+++ js/main.js
@@ -73,7 +73,7 @@ $(document).ready(function(){
     $('#fullpage').fullpage({
         sectionSelector: '.b-screen',
         navigation: true,
-        navigationPosition: '',
+        navigationPosition: 'right',
         /*onLeave: function(index, nextIndex, direction){console.log("++");},
         afterLoad: function(anchorLink, index){console.log("++");},
         afterRender: function(){console.log("++");},
@@ -83,44 +83,139 @@ $(document).ready(function(){
         onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){console.log("++");}*/
     });
 
-    $('#fp-nav ul').wrap("<div class='b-block'></div>");
-
     $('.b-screen-about').prepend( $('.fp-tableCell .b-tube'));
 
-    console.log("init");
-	// var myPlace = new google.maps.LatLng(55.754407, 37.625151);
- //    var myOptions = {
- //        zoom: 16,
- //        center: myPlace,
- //        mapTypeId: google.maps.MapTypeId.ROADMAP,
- //        disableDefaultUI: true,
- //        scrollwheel: false,
- //        zoomControl: true
- //    }
- //    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions); 
+    if($('.main-page').length){
+        $('.b-logo').on('click', function(){
+            $.fn.fullpage.moveTo(1);
+            return false;
+        });
+    }
 
- //    var marker = new google.maps.Marker({
-	//     position: myPlace,
-	//     map: map,
-	//     title: "Ярмарка вакансий и стажировок"
-	// });
+    var styles = [
+        {
+            "featureType": "all",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "saturation": -100
+                },
+                {
+                    "gamma": 1
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "saturation": "-99"
+                },
+                {
+                    "lightness": "38"
+                },
+                {
+                    "gamma": "3.11"
+                },
+                {
+                    "color": "#aaaaaa"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "geometry",
+            "stylers": [
+                { "color": "#fece0b" },
+                { "visibility": "simplified" }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#000000"
+                }
+            ]
+        }
+    ];
 
-    //  var options = {
-    //     $AutoPlay: true,                                
-    //     $SlideDuration: 500,                            
+    function mapInfo($btn)
+    {
+        this.name = $btn.attr("data-map");
+        this.x = parseFloat($btn.attr("data-coord-x"));
+        this.y = parseFloat($btn.attr("data-coord-y"));
+        this.myPlace = new google.maps.LatLng(this.x, this.y);
+        var myOptions = {
+            zoom: 16,
+            center: this.myPlace,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            disableDefaultUI: true,
+            scrollwheel: false,
+            zoomControl: true
+        }
+        this.map = new google.maps.Map(document.getElementById(this.name), myOptions);
 
-    //     $BulletNavigatorOptions: {                      
-    //         $Class: $JssorBulletNavigator$,             
-    //         $ChanceToShow: 2,                           
-    //         $AutoCenter: 1,                            
-    //         $Steps: 1,                                  
-    //         $Lanes: 1,                                  
-    //         $SpacingX: 10,                              
-    //         $SpacingY: 10,                              
-    //         $Orientation: 1                             
-    //     }
-    // };
+        this.styledMap = new google.maps.StyledMapType(styles,{name: "Styled Map"});
+        this.map.mapTypes.set('map_style', this.styledMap);
+        this.map.setMapTypeId('map_style');
 
-    // var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+        this.marker = new google.maps.Marker({
+            position: this.myPlace,
+            map: this.map,
+            icon: {
+                url: "i/pin.svg",
+                scaledSize: new google.maps.Size(40, 58), // scaled size
+                origin: new google.maps.Point(0,0), // origin
+                anchor: new google.maps.Point(23,50), // anchor
+            },
+            title: "Офис"
+        });
+        this.marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+
+    //массив со всеми картами
+    var maps = [];
+
+    $('.b-btn-city').each(function(){
+        maps.push(new mapInfo($(this)));
+    });
+
+    $('.b-map-city a').on('click', function(){
+        $this = $(this);
+        $this.siblings("a").each(function(){
+            var block = $(this).attr("data-map");
+            $('#'+block).addClass("hide");
+            $(this).removeClass("active");
+        });
+        var block = $this.attr("data-map");
+        $('#'+block).removeClass("hide");
+        $this.addClass("active");
+
+        //Обновить карту
+        var mapObj = maps[$this.index()];
+        google.maps.event.trigger(mapObj.map, 'resize');
+        mapObj.map.setCenter(mapObj.myPlace);
+    });
+
+    /*var options = {
+        $AutoPlay: true,                                
+        $SlideDuration: 500,                            
+
+        $BulletNavigatorOptions: {                      
+            $Class: $JssorBulletNavigator$,             
+            $ChanceToShow: 2,                           
+            $AutoCenter: 1,                            
+            $Steps: 1,                                  
+            $Lanes: 1,                                  
+            $SpacingX: 10,                              
+            $SpacingY: 10,                              
+            $Orientation: 1                             
+        }
+    };
+
+    var jssor_slider1 = new $JssorSlider$("slider1_container", options);*/
 
 });
