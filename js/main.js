c37b337b7c10587663c5ec5ba66e548d4d8e5f14
rom4es <rom4es_1993@mail.ru>
2018-01-06 15:57:47 +0700

    123

--- js/main.js
+++ js/main.js
@@ -334,6 +334,20 @@ $(document).ready(function(){
             $('#slider-nav a[data-id="'+slideID+'"]').click();
             blockHash = false;
         }
+
+        var myWidth, myHeight;
+         if( typeof( window.innerWidth ) == 'number' ) {
+            myWidth = window.innerWidth;
+            myHeight = window.innerHeight;
+        } else if( document.documentElement && ( document.documentElement.clientWidth || 
+        document.documentElement.clientHeight ) ) {
+            myWidth = document.documentElement.clientWidth;
+            myHeight = document.documentElement.clientHeight;
+        } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+            myWidth = document.body.clientWidth;
+            myHeight = document.body.clientHeight;
+        }
+        alert("myWidth = "+myWidth+" /// myHeight = "+ myHeight);
     });
 
     var styles = [
